b2e6b7f8718bd8eed4889e6412e928a34bfa93e0
Michael Gibson <96648835+Smcilegend@users.noreply.github.com>
2024-06-26 10:42:07 -0700

    Add files via upload

---
+++ Online Quiz App-questions.js
@@ -0,0 +1,59 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What is your favorite programming language?",
+        answer: "Javascript",
+        options: [
+            "HTML",
+            "Wapo",
+            "Javascript",
+            "Cascading Style Sheet"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What holds the title element in an HTML Project?",
+        answer: "head",
+        options: [
+            "span",
+            "src",
+            "head",
+            "img"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Which 'ONE' of the answers is a Data Type?",
+        answer: "Boolean",
+        options: [
+            "Boolean",
+            "Unpreferred",
+            "Registered",
+            "pull"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which one of the Comparison Operaters means 'Equal to'?",
+        answer: "==",
+        options: [
+            "+=",
+            "==",
+            "===",
+            "!="
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which one of these symbols creates an object?",
+        answer: "{}",
+        options: [
+            "[]",
+            "{}",
+            "||",
+            "<="
+        ]
+    }
+];
+
+  
